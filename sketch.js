e26019a40bd1f989d204ffa8967512aa6a5811b2
Yiwen115 <yiwen951127@gmail.com>
2025-04-28 10:18:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ function setup() {
   background('#ffc2d1'); // 設定背景顏色
   capture = createCapture(VIDEO); // 擷取攝影機影像
   capture.size(windowWidth * 0.8, windowHeight * 0.8); // 設定影像大小為視窗的 80%
+  capture.mirror(false); // 關閉鏡像效果
   capture.hide(); // 隱藏原始攝影機影像
 }
 
@@ -13,11 +14,7 @@ function draw() {
   let x = (width - capture.width) / 2; // 計算影像的水平居中位置
   let y = (height - capture.height) / 2; // 計算影像的垂直居中位置
 
-  push(); // 儲存當前繪圖設定
-  translate(x + capture.width, y); // 將畫布原點移動到影像右側
-  scale(-1, 1); // 水平翻轉影像
-  image(capture, 0, 0, capture.width, capture.height); // 繪製攝影機影像
-  pop(); // 恢復繪圖設定
+  image(capture, x, y, capture.width, capture.height); // 繪製攝影機影像
 }
 
 function windowResized() {
